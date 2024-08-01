188436119be5b3f16839042e9fddd5e92596a09b
aammddkkzxc <ideal0427@naver.com>
2024-08-02 02:20:16 +0900

    feat: 타입 매개변수 제한4 - 타입 매개변수 제한 - 타입 매개변수 상한을 사용해서 타입 안전성을 지키면서 상위 타입의 원하는 기능까지 사용할 수 있다. - 자바 컴파일러는 T 에 입력될 수 있는 값의 범위를 예측할 수 있다.

--- src/generic/ex3/AnimalHospitalV2.java
+++ src/generic/ex3/AnimalHospitalV2.java
@@ -9,14 +9,14 @@ public class AnimalHospitalV2<T> {
         this.animal = animal;
     }
 
-    public void checkup() {
-        //T의 타입을 메서드를 정의하는 시점에는 알 수가 없음... (Object기능만 사용) => 컴파일 오류
-        System.out.println("동물 이름: " + animal.getName());
-        System.out.println("동물 크기: " + animal.getSize());
-        animal.sound();
-    }
-
-    public T getBigger(Animal target) {
-        return animal.getSize() > target.getSize() ? animal : target;
-    }
+//    public void checkup() {
+//        //T의 타입을 메서드를 정의하는 시점에는 알 수가 없음... (Object기능만 사용) => 컴파일 오류
+//        System.out.println("동물 이름: " + animal.getName());
+//        System.out.println("동물 크기: " + animal.getSize());
+//        animal.sound();
+//    }
+//
+//    public T getBigger(Animal target) {
+//        return animal.getSize() > target.getSize() ? animal : target;
+//    }
 }
