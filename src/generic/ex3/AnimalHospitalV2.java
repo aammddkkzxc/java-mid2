4540ff7f560e6572fe920b87411089a1fff6c2cb
aammddkkzxc <ideal0427@naver.com>
2024-08-02 01:56:15 +0900

    feat: 타입 매개변수 제한3 - 제네릭 도입과 실패 - 자바 컴파일러는 어떤 타입이 들어올 지 알 수 없기 때문에 T 를 어떤 타입이든 받을 수 있는 모든 객체의 최종 부모인 Object 타입으로 가정한다. 따라서 Object 가 제공하는 메서드만 호출할 수 있다. - 원하는 기능을 사용하려면 Animal 타입이 제공하는 기능들이 필요한데, 이 기능을 모두 사용할 수 없다. - 동물 병원에 Integer , Object 같은 동물과 전혀 관계 없는 타입을 타입 인자로 전달 할 수 있다는 문제

---
+++ src/generic/ex3/AnimalHospitalV2.java
@@ -0,0 +1,22 @@
+package generic.ex3;
+
+import generic.animal.Animal;
+
+public class AnimalHospitalV2<T> {
+    private T animal;
+
+    public void set(T animal) {
+        this.animal = animal;
+    }
+
+    public void checkup() {
+        //T의 타입을 메서드를 정의하는 시점에는 알 수가 없음... (Object기능만 사용) => 컴파일 오류
+        System.out.println("동물 이름: " + animal.getName());
+        System.out.println("동물 크기: " + animal.getSize());
+        animal.sound();
+    }
+
+    public T getBigger(Animal target) {
+        return animal.getSize() > target.getSize() ? animal : target;
+    }
+}
