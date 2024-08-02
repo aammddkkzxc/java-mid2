d4f78199732fa8daa15969f87c3b722b32207bfb
aammddkkzxc <ideal0427@naver.com>
2024-08-02 15:58:43 +0900

    feat: 타입 이레이저 - 제네릭은 자바 컴파일 단계에서만 사용되고, 컴파일 이후에는 제네릭 정보가 삭제된다. - java 에는 제네릭의 타입 매개변수가 존재하지만, 컴파일 이후인 자바 바이트코드 .class 에는 타입 매개변수가 존재하지 않음 - 상한 제한 없는 T는 Object로, 상한 제한 이 Animal클래스라면 Animal로 변환된다. - instanceof 는 항상 Object 와 비교하게 된다. 이렇게 되면 항상 참이반환되는 문제가 발생한다. 자바는 이런 문제 때문에 타입 매개변수에 instanceof 를 허용하지 않는다. - new T 는 항상 new Object 가 되어버린다. 개발자가 의도한 것과는 다르다. 따라서 자바는 타입 매개변수에   new 를 허용하지 않는다.

---
+++ src/generic/ex5/EraserBox.java
@@ -0,0 +1,13 @@
+package generic.ex5;
+
+public class EraserBox<T> {
+
+    public boolean instanceOf(Object param) {
+        return param instanceof T; //오류
+    }
+
+    public void create() {
+        return new T(); //오류
+    }
+
+}
