0fd746ce1ed5d6f08a0f921c27d060facff96ae8
aammddkkzxc <ideal0427@naver.com>
2024-08-04 20:28:28 +0900

    feat: 문제와 풀이4 - Queue

---
+++ src/collection/deque/test/queue/TaskScheduler.java
@@ -0,0 +1,22 @@
+package collection.deque.test.queue;
+
+import java.util.*;
+
+public class TaskScheduler {
+    private Queue<Task> tasks = new ArrayDeque<>();
+
+    public void addTask(Task task) {
+        tasks.offer(task);
+    }
+
+    public int getRemainingTasks() {
+        return tasks.size();
+    }
+
+    public void processNextTask() {
+        Task task = tasks.poll();
+        if (task != null) {
+            task.execute();
+        }
+    }
+}
