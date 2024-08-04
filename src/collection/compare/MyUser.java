ea7e700bad0f148606b584784310a7fd86abf2e7
aammddkkzxc <ideal0427@naver.com>
2024-08-04 23:25:28 +0900

    feat: 정렬2 - Comparable, Comparator - 객체의 기본 정렬 방법은 객체에 Comparable 를 구현해서 정의한다. 이렇게 하면 객체는 이름 그대로 비교할 수 있는 객체가 되고 기본 정렬 방법을 가진다. - 그런데 기본 정렬 외에 다른 정렬 방법을 사용해야 하는 경우 비교자 ( Comparator )를 별도로 구현해서 정렬 메서드에 전달하면 된다. 이 경우 전달한 Comparator 가 항상 우선권을 가진다.

---
+++ src/collection/compare/MyUser.java
@@ -0,0 +1,24 @@
+package collection.compare;
+
+public class MyUser implements Comparable<MyUser> {
+    private final String id;
+    private final int age;
+
+    public MyUser(String id, int age) {
+        this.id = id;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(MyUser o) {
+        return this.age < o.age ? -1 : (this.age == o.age ? 0 : 1);
+    }
+
+    @Override
+    public String toString() {
+        return "MyUser{" +
+                "name='" + id + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
